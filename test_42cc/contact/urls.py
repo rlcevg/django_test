37a4d06a1c682e5fc1f4a3bb73af832ec470f10f
rlcevg <rlcevg@gmail.com>
2010-12-28 16:01:15 +0200

    ticket:1 Changed generic list view to generic detail view

--- test_42cc/contact/urls.py
+++ test_42cc/contact/urls.py
@@ -3,8 +3,9 @@ from contact.models import Person
 
 info_dict = {
     'queryset': Person.objects.all(),
+    'object_id': 1,
 }
 
 urlpatterns = patterns('',
-    (r'^$', 'django.views.generic.list_detail.object_list', info_dict),
+    (r'^$', 'django.views.generic.list_detail.object_detail', info_dict),
 )
