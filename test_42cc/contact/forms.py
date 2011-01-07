d3d5336511fe5ad626e090471396a72bf69a4e97
rlcevg <rlcevg@gmail.com>
2011-01-07 09:57:38 +0200

    ticket:5  Created forms

---
+++ test_42cc/contact/forms.py
@@ -0,0 +1,11 @@
+from django.forms import ModelForm
+from contact.models import Person, Contact
+from django.forms.models import inlineformset_factory
+
+
+class PersonForm(ModelForm):
+    class Meta:
+        model = Person
+        exclude = ('signin_date')
+
+AboutFormSet = inlineformset_factory(Person, Contact)
