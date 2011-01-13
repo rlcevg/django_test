8d5b32a5b34908f2a1f689d557b04aa004c2a1af
rlcevg <rlcevg@gmail.com>
2011-01-13 14:27:38 +0200

    ticket:6  Added Media data to form

--- test_42cc/contact/forms.py
+++ test_42cc/contact/forms.py
@@ -1,6 +1,8 @@
 from django.forms import ModelForm, Textarea
 from contact.models import Person, Contact
 from django.forms.models import inlineformset_factory
+from contact.widgets import CalendarWidget
+from django.conf import settings
 
 
 class PersonForm(ModelForm):
@@ -9,6 +11,29 @@ class PersonForm(ModelForm):
         exclude = ('signin_date')
         widgets = {
             'biography': Textarea(attrs={'cols': 80, 'rows': 20}),
+            'birth_date': CalendarWidget(attrs={
+                'img': settings.SITE_MEDIA_PREFIX + "img/baloon_24.png",
+            }),
         }
 
-ContactFormSet = inlineformset_factory(Person, Contact, max_num=5, extra=2)
+    class Media:
+        css = {
+            'all': (settings.SITE_MEDIA_PREFIX + "css/edit.css",),
+        }
+        js = (
+            settings.SITE_MEDIA_PREFIX + "js/jquery.min.js",
+            settings.SITE_MEDIA_PREFIX + "js/jquery.form.js",
+            settings.SITE_MEDIA_PREFIX + "js/ajax_person_form.js",
+        )
+
+
+class ContactForm(ModelForm):
+    class Meta:
+        model = Contact
+        widgets = {
+            'contact_info': Textarea(attrs={'cols': 40, 'rows': 2})
+        }
+
+
+ContactFormSet = inlineformset_factory(Person, Contact, form=ContactForm,
+        max_num=5, extra=2)
