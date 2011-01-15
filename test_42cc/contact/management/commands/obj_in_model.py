cf1cb519a982492a61077f269ad2ec03980be241
rlcevg <rlcevg@gmail.com>
2011-01-15 22:11:24 +0200

    ticket:9  Added test

--- test_42cc/contact/management/commands/obj_in_model.py
+++ test_42cc/contact/management/commands/obj_in_model.py
@@ -9,8 +9,9 @@ class Command(NoArgsCommand):
     def handle_noargs(self, **options):
         self.stdout.write('Total count of objects in:\n'.format())
         for model in models.get_models():
-            self.stdout.write('{0}.{1}:  {2}\n'.format(
+            self.stdout.write('{0}.{1} (fields: {2}, objects: {3})\n'.format(
                     model.__module__,
                     model.__name__,
+                    len(model._meta.fields),
                     model._default_manager.count(),
                 ))
