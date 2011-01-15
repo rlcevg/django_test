784ded67aaa908701b03184b584caf3941afacc1
rlcevg <rlcevg@gmail.com>
2011-01-15 13:35:46 +0200

    ticket:8  Added admin urls

--- test_42cc/urls.py
+++ test_42cc/urls.py
@@ -1,22 +1,14 @@
 from django.conf.urls.defaults import *
 from django.conf import settings
+from django.contrib import admin
 import os
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+admin.autodiscover()
 
 urlpatterns = patterns('',
     (r'^', include('contact.urls')),
     (r'^' + settings.SITE_MEDIA_PREFIX[1:] + '(?P<path>.*)$',
             'django.views.static.serve',
             {'document_root': os.path.join(settings.CURRENT_PATH, 'media')}),
-    # Example:
-    # (r'^test_42cc/', include('test_42cc.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
+    (r'^admin/', include(admin.site.urls)),
 )
