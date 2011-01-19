4cd281da6079e84938e45dda1db3a90172578cdf
rlcevg <rlcevg@gmail.com>
2011-01-19 18:50:34 +0200

    ticket:8  minor fixes

--- test_42cc/contact/templatetags/contact_extra.py
+++ test_42cc/contact/templatetags/contact_extra.py
@@ -1,19 +1,23 @@
 from django import template
+from django.db import models
 from django.core import urlresolvers
 from django.utils.safestring import mark_safe
 
 
 def edit_link(obj):
-    object_name = obj.__unicode__()
-    url = urlresolvers.reverse(
-        'admin:{0}_{1}_change'.format(
-            obj._meta.app_label,
-            obj._meta.module_name
-        ),
-        args=(obj.id,)
-    )
-    return mark_safe(u'Edit <a href="{0}">{1}</a>'.format(
-            url, object_name))
+    if isinstance(obj, models.Model) and obj.pk:
+        object_name = obj.__unicode__()
+        url = urlresolvers.reverse(
+            'admin:{0}_{1}_change'.format(
+                obj._meta.app_label,
+                obj._meta.module_name
+            ),
+            args=(obj.id,)
+        )
+        return mark_safe(u'Edit <a href="{0}">{1}</a>'.format(
+                url, object_name))
+    else:
+        return ''
 
 
 register = template.Library()
